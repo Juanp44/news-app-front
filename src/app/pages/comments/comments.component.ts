5723bc282a35c407a640b27da735f4b5616dff1e
Juan Pablo <juanpablo@MacBook-de-LUIS.local>
2020-11-01 14:18:47 -0600

    Update: Added a 404 page and comments component as well as router

---
+++ src/app/pages/comments/comments.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+
+  newsId:number;
+
+  constructor(private activatedRoute:ActivatedRoute) {
+    this.activatedRoute.params.subscribe(params =>{
+      console.log("Response : ",  params);
+      this.newsId = params.id;
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+}
